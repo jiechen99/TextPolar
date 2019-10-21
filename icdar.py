ae94bb5857a11f786de46689f4b197cc461d2a8c
Gao Yichen <yichenirving@gmail.com>
2019-10-21 18:18:54 +0800

    修改采样部分代码，提高背景选取的效率

--- icdar.py
+++ icdar.py
@@ -74,7 +74,70 @@ def load_annoataion(p):
 
         return np.array(text_polys, dtype=np.float32), np.array(text_tags, dtype=np.bool)
 
-def crop_area(im, polys, tags, crop_background=False, max_tries=50):
+# def crop_area(im, polys, tags, crop_background=False, max_tries=50):
+#     '''
+#     make random crop from the input image
+#     :param im:
+#     :param polys:
+#     :param tags:
+#     :param crop_background:
+#     :param max_tries:
+#     :return:
+#     '''
+#     h, w, _ = im.shape
+#     pad_h = h//10
+#     pad_w = w//10
+#     h_array = np.zeros((h + pad_h*2), dtype=np.int32)
+#     w_array = np.zeros((w + pad_w*2), dtype=np.int32)
+#     for poly in polys:
+#         poly = np.round(poly, decimals=0).astype(np.int32)
+#         minx = np.min(poly[:, 0])
+#         maxx = np.max(poly[:, 0])
+#         w_array[minx+pad_w:maxx+pad_w] = 1
+#         miny = np.min(poly[:, 1])
+#         maxy = np.max(poly[:, 1])
+#         h_array[miny+pad_h:maxy+pad_h] = 1
+#     # ensure the cropped area not across a text
+#     h_axis = np.where(h_array == 0)[0]
+#     w_axis = np.where(w_array == 0)[0]
+#     if len(h_axis) == 0 or len(w_axis) == 0:
+#         return im, polys, tags
+#     for i in range(max_tries):
+#         xx = np.random.choice(w_axis, size=2)
+#         xmin = np.min(xx) - pad_w
+#         xmax = np.max(xx) - pad_w
+#         xmin = np.clip(xmin, 0, w-1)
+#         xmax = np.clip(xmax, 0, w-1)
+#         yy = np.random.choice(h_axis, size=2)
+#         ymin = np.min(yy) - pad_h
+#         ymax = np.max(yy) - pad_h
+#         ymin = np.clip(ymin, 0, h-1)
+#         ymax = np.clip(ymax, 0, h-1)
+#         if xmax - xmin < FLAGS.min_crop_side_ratio*w or ymax - ymin < FLAGS.min_crop_side_ratio*h:
+#             # area too small
+#             continue
+#         if polys.shape[0] != 0:
+#             poly_axis_in_area = (polys[:, :, 0] >= xmin) & (polys[:, :, 0] <= xmax) \
+#                                 & (polys[:, :, 1] >= ymin) & (polys[:, :, 1] <= ymax)
+#             selected_polys = np.where(np.sum(poly_axis_in_area, axis=1) == 14)[0]
+#         else:
+#             selected_polys = []
+#         if len(selected_polys) == 0:
+#             # no text in this area
+#             if crop_background:
+#                 return im[ymin:ymax+1, xmin:xmax+1, :], polys[selected_polys], tags[selected_polys]
+#             else:
+#                 continue
+#         im = im[ymin:ymax+1, xmin:xmax+1, :]
+#         polys = polys[selected_polys]
+#         tags = tags[selected_polys]
+#         polys[:, :, 0] -= xmin
+#         polys[:, :, 1] -= ymin
+#         return im, polys, tags
+
+#     return im, polys, tags
+
+def crop_area(im, polys, tags, crop_background=False, max_tries=500):
     '''
     make random crop from the input image
     :param im:
@@ -85,57 +148,85 @@ def crop_area(im, polys, tags, crop_background=False, max_tries=50):
     :return:
     '''
     h, w, _ = im.shape
-    pad_h = h//10
-    pad_w = w//10
-    h_array = np.zeros((h + pad_h*2), dtype=np.int32)
-    w_array = np.zeros((w + pad_w*2), dtype=np.int32)
-    for poly in polys:
-        poly = np.round(poly, decimals=0).astype(np.int32)
-        minx = np.min(poly[:, 0])
-        maxx = np.max(poly[:, 0])
-        w_array[minx+pad_w:maxx+pad_w] = 1
-        miny = np.min(poly[:, 1])
-        maxy = np.max(poly[:, 1])
-        h_array[miny+pad_h:maxy+pad_h] = 1
-    # ensure the cropped area not across a text
-    h_axis = np.where(h_array == 0)[0]
-    w_axis = np.where(w_array == 0)[0]
-    if len(h_axis) == 0 or len(w_axis) == 0:
-        return im, polys, tags
-    for i in range(max_tries):
-        xx = np.random.choice(w_axis, size=2)
-        xmin = np.min(xx) - pad_w
-        xmax = np.max(xx) - pad_w
-        xmin = np.clip(xmin, 0, w-1)
-        xmax = np.clip(xmax, 0, w-1)
-        yy = np.random.choice(h_axis, size=2)
-        ymin = np.min(yy) - pad_h
-        ymax = np.max(yy) - pad_h
-        ymin = np.clip(ymin, 0, h-1)
-        ymax = np.clip(ymax, 0, h-1)
-        if xmax - xmin < FLAGS.min_crop_side_ratio*w or ymax - ymin < FLAGS.min_crop_side_ratio*h:
-            # area too small
-            continue
-        if polys.shape[0] != 0:
+    if crop_background:
+        h_axis = np.arange(0, h-1)
+        w_axis = np.arange(0, w-1)
+        for i in range(max_tries):
+            xx = np.random.choice(w_axis, size=2)
+            xmin = np.min(xx)
+            xmax = np.max(xx)
+            yy = np.random.choice(h_axis, size=2)
+            ymin = np.min(yy)
+            ymax = np.max(yy)
+
+            if xmax - xmin < FLAGS.min_crop_side_ratio*w or ymax - ymin < FLAGS.min_crop_side_ratio*h:
+                # the croped image is too small
+                continue
+                
             poly_axis_in_area = (polys[:, :, 0] >= xmin) & (polys[:, :, 0] <= xmax) \
                                 & (polys[:, :, 1] >= ymin) & (polys[:, :, 1] <= ymax)
-            selected_polys = np.where(np.sum(poly_axis_in_area, axis=1) == 14)[0]
-        else:
-            selected_polys = []
-        if len(selected_polys) == 0:
-            # no text in this area
-            if crop_background:
-                return im[ymin:ymax+1, xmin:xmax+1, :], polys[selected_polys], tags[selected_polys]
-            else:
+            if np.any(poly_axis_in_area):
                 continue
-        im = im[ymin:ymax+1, xmin:xmax+1, :]
-        polys = polys[selected_polys]
-        tags = tags[selected_polys]
-        polys[:, :, 0] -= xmin
-        polys[:, :, 1] -= ymin
-        return im, polys, tags
+            else:
+                return im[ymin:ymax+1, xmin:xmax+1, :], np.array([]), np.array([])
+    else:
+        # 防止无法取到边界
+        pad_h = h//10
+        pad_w = w//10
+        h_array = np.zeros((h + pad_h*2), dtype=np.int32)
+        w_array = np.zeros((w + pad_w*2), dtype=np.int32)
+        for poly in polys:
+            # 已经进行过下取整操作，这里无需再进行round，只需要将其转换为int即可
+            # poly = np.round(poly, decimals=0).astype(np.int32)
+            minx = np.min(poly[:, 0].astype(np.int32))
+            maxx = np.max(poly[:, 0].astype(np.int32))
+            w_array[minx+pad_w:maxx+pad_w] = 1
+            miny = np.min(poly[:, 1].astype(np.int32))
+            maxy = np.max(poly[:, 1].astype(np.int32))
+            h_array[miny+pad_h:maxy+pad_h] = 1
+        # ensure the cropped area not across a text
+        h_axis = np.where(h_array == 0)[0]
+        w_axis = np.where(w_array == 0)[0]
+        # 如果全图中没有有效范围，则直接返回全图
+        if len(h_axis) == 0 or len(w_axis) == 0:
+            return im, polys, tags
+        for i in range(max_tries):
+            xx = np.random.choice(w_axis, size=2)
+            xmin = np.min(xx) - pad_w
+            xmax = np.max(xx) - pad_w
+            xmin = np.clip(xmin, 0, w-1)
+            xmax = np.clip(xmax, 0, w-1)
+            yy = np.random.choice(h_axis, size=2)
+            ymin = np.min(yy) - pad_h
+            ymax = np.max(yy) - pad_h
+            ymin = np.clip(ymin, 0, h-1)
+            ymax = np.clip(ymax, 0, h-1)
 
+            if xmax - xmin < FLAGS.min_crop_side_ratio*w or ymax - ymin < FLAGS.min_crop_side_ratio*h:
+                # the croped image is too small
+                continue
+            # 这个if很迷，暂且留着
+            if polys.shape[0] != 0:
+                poly_axis_in_area = (polys[:, :, 0] >= xmin) & (polys[:, :, 0] <= xmax) \
+                                    & (polys[:, :, 1] >= ymin) & (polys[:, :, 1] <= ymax)
+                # print(poly_axis_in_area)
+                selected_polys = np.where(np.all(poly_axis_in_area, axis=1) == True)[0]
+            else:
+                selected_polys = []
+            if len(selected_polys) == 0:
+                # no text in this area
+                continue
+            else:
+                im = im[ymin:ymax+1, xmin:xmax+1, :]
+                polys = polys[selected_polys]
+                tags = tags[selected_polys]
+                polys[:, :, 0] -= xmin
+                polys[:, :, 1] -= ymin
+                return im, polys, tags
+            
+    # 如果没有取到符合要求的图像，需要返回原图，并需在调用处继续判断
     return im, polys, tags
+
 
 def getCenterLine(pList):
     length = len(pList)
@@ -397,15 +488,15 @@ def generator(input_size=512, batch_size=32,
 
                 # added by YCIrving
                 # 这里需要下取整，如果scale=0.5，则对于在边界上的像素点，如果其坐标为奇数
-                # 则变换后取round会越界(crop_area函数中的np.round())                
+                # 则变换后取round会越界(crop_area函数中的np.round())
                 text_polys = np.floor(text_polys * rd_scale)
-                
+
                 # print rd_scale
 
                 # random crop a area from image
                 if np.random.rand() < background_ratio:
                     # crop background
-                    im, text_polys, text_tags = crop_area(im, text_polys, text_tags, crop_background=True)
+                    im, text_polys, text_tags = crop_area(im, text_polys, text_tags, crop_background=True, )
                     if text_polys.shape[0] > 0:
                         # cannot find background
                         continue
